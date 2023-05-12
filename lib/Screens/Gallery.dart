b5be914a45467df66970548b726843701a50436a
Abik Vaidhya <abikvaidhya@gmail.com>
2023-05-12 22:49:17 +0545

    changes
    - gallery update

--- lib/Screens/Gallery.dart
+++ lib/Screens/Gallery.dart
@@ -10,7 +10,7 @@ class GalleryScreen extends StatelessWidget {
       child: Column(
         children: [
           Text(
-            "i'm abik vaidhya, from pokhara. i'm 24 years young ( cuz i dont wanna feel old T_T ). i'm a graduate from informatics college pokhara where i graduated on second class honors on bachelors in information technology.",
+            "i'm abik vaidhya, from Pokhara, Nepal. i'm a graduate from informatics college pokhara where i graduated on second class honors on bachelors in information technology.",
             style: TextStyle(fontSize: 20),
           ),
           Divider(
@@ -18,7 +18,7 @@ class GalleryScreen extends StatelessWidget {
             color: Colors.transparent,
           ),
           Text(
-            "currently im studying part-time masters in business and administration in herald international college. i work as a remote flutter developer in nipunna sewa, nucleus in pokhara. i handle frontedn for the projects outsourced from dubai team to our company.",
+            "currently im studying part-time masters in business and administration in herald international college. i work as a remote flutter developer in nipunna sewa, nucleus in pokhara. i handle frontend for the projects outsourced from dubai team to our company.",
             style: TextStyle(fontSize: 20),
           ),
           Divider(
@@ -26,7 +26,7 @@ class GalleryScreen extends StatelessWidget {
             color: Colors.transparent,
           ),
           Text(
-            "i stream games in my leisure time, as regular as i can. i play valorant, apex legends, pubg pc, minecraft, gta v online and whichever games that epic games provides free as their giveaway ( cuz im broke and cant buy shit )",
+            "i stream games in my leisure time, as regular as i can. i play valorant, apex legends, pubg pc, minecraft, gta v online and whichever games that epic games provides free as their giveaway.",
             style: TextStyle(fontSize: 20),
           ),
         ],
